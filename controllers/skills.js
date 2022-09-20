317493dc4ab910d1638841525c69e28fd9c495f6
James Hudson <jmh357@comcast.net>
2022-09-20 18:28:51 -0500

    add controller fxn and refactor skills router. Complete Part 1 of lab

---
+++ controllers/skills.js
@@ -0,0 +1,12 @@
+// import the Skill data
+import { skills } from '../data/skill-data.js'
+
+function index (req, res) {
+  res.render('skills/index', {
+    skills: skills
+  })
+}
+
+export {
+  index
+}
