fcd7a285f902d4f10a6315b56f93b09e76f91ba6
James Hudson <jmh357@comcast.net>
2022-09-20 17:55:57 -0500

    add skills data file and skills index.ejs file

---
+++ data/skill-data.js
@@ -0,0 +1,12 @@
+const skills = [
+  {type: 'HTML', unit: 1, completed: true},
+  {type: 'CSS', unit: 1, completed: true},
+  {type: 'JavaScript', unit: 1, completed: true},
+  {type: 'MongoDB', unit: 2, completed: false},
+  {type: 'Express', unit: 2, completed: false},
+  {type: 'Node', unit: 2, completed: false},
+]
+
+export {
+  skills
+}
