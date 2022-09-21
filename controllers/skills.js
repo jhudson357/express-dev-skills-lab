65a619e191f2c4efa98c1b489e99fcbfec0893ef
James Hudson <jmh357@comcast.net>
2022-09-20 19:37:22 -0500

    refactor the index controller fxn. Complete part 2 of lab.

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,19 @@
-// import the Skill data
-import { skills } from '../data/skill-data.js'
+//// import the Skill data
+//// import { skills } from '../data/skill-data.js'
+
+// import the Skill model file
+import { Skill } from '../models/skill.js'
 
 function index (req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills
+    })
+  })
+  .catch(error => {
+    console.log('error')
+    res.redirect('/')
   })
 }
 
